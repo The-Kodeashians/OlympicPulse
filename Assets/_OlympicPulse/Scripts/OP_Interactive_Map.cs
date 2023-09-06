685d594eb49160f7f363fe17ab2728dea37e6141
Luke Hewitt <lukehewitt@smashit.games>
2023-09-06 14:56:24 +1000

    Interactive Map now instantiates on Touch
    And touching again destroys the 3D object

--- Assets/_OlympicPulse/Scripts/OP_Interactive_Map.cs
+++ Assets/_OlympicPulse/Scripts/OP_Interactive_Map.cs
@@ -8,14 +8,44 @@ public class OP_Interactive_Map : MonoBehaviour
     public ARPlaneManager arPlaneManager;
     public GameObject objectToPlace;
 
-    // This method is called whenever a new plane is detected
+    private GameObject currentObjectInstance; // This will store our instantiated object
+
+    void Update()
+    {
+        // Check for screen touches
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            ToggleMap();
+        }
+    }
+
+    void ToggleMap()
+    {
+        // If the map is not instantiated, instantiate it
+        if (currentObjectInstance == null)
+        {
+            List<ARPlane> allPlanes = new List<ARPlane>();
+            foreach (var plane in arPlaneManager.trackables)
+            {
+                allPlanes.Add(plane);
+            }
+
+            if (allPlanes.Count > 0) 
+            {
+                currentObjectInstance = Instantiate(objectToPlace, allPlanes[0].center, Quaternion.identity);
+            }
+        }
+        // If the map is already instantiated, destroy it
+        else
+        {
+            Destroy(currentObjectInstance);
+            currentObjectInstance = null;
+        }
+    }
+
     void OnPlaneAdded(ARPlanesChangedEventArgs args)
     {
-        foreach (var newPlane in args.added)
-        {
-            // Instantiate objectToPlace at the centre of the detected plane
-            Instantiate(objectToPlace, newPlane.center, Quaternion.identity);
-        }
+        // We won't automatically instantiate the object here anymore
     }
 
     void OnEnable()
