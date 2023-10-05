94e1a67c7ea56170d6d5ba73de45ccb47a99917c
Luke Hewitt <lukehewitt@smashit.games>
2023-10-06 09:54:15 +1000

    Sprinting time reduction & simplified math
    - Changed from 100 metres to 20metres
    - Replaced the original pcae factor logic with new calculations based on world record times.
    - Renamed `WorldRecordPace` to `WorldRecordTime100m` to better reflect its meaning.
    - Calculated the time it would take to complete 20 metres based on the world record time for 100 metres.
    - Calculated the speed for hte 20-metre sprint using the new time.
    - Updated `UpdateRunnerPosition()` method to move the sprinter based on the newly calculated speed.

--- Assets/_OlympicPulse/Scripts/OP_AR_Sprint_Controller.cs
+++ Assets/_OlympicPulse/Scripts/OP_AR_Sprint_Controller.cs
@@ -13,7 +13,7 @@ namespace _OlympicPulse.Scripts
         public GameObject SprinterPrefab;
 
         [Header("Race Settings")]
-        public float WorldRecordPace;
+        public float WorldRecordTime100m;
         public float RaceCountdownDuration = 3.0f;
 
         [Header("UI Elements")]
@@ -30,7 +30,7 @@ namespace _OlympicPulse.Scripts
         private float totalDistanceCovered = 0.0f;
         private bool raceEnded = false;
 
-        private Vector3 initialPosition;  // To keep track of the initial position of the sprinter
+        private Vector3 initialPosition;
 
         private void Awake()
         {
@@ -60,7 +60,7 @@ namespace _OlympicPulse.Scripts
             {
                 CountdownText.text = Mathf.Ceil(raceCountdown).ToString();
             }
-            // Start the animation at 1 second remaining
+
             if (raceCountdown <= 1.0f && !spawnedSprinter.GetComponent<Actions>().IsAnimating("Run"))
             {
                 if (spawnedSprinter)
@@ -68,7 +68,7 @@ namespace _OlympicPulse.Scripts
                     spawnedSprinter.GetComponent<Actions>().Run();
                 }
             }
-            // End the countdown and start the race
+
             if (raceCountdown <= 0)
             {
                 if (CountdownText != null)
@@ -78,11 +78,10 @@ namespace _OlympicPulse.Scripts
                 raceTimer = 0.01f;
             }
         }
-        
+
         private void RunRace()
         {
             raceTimer += Time.deltaTime;
-            Debug.Log($"Sprinter Position at RunRace(): {spawnedSprinter.transform.position} and at raceTimer: {raceTimer}");
             UpdateRunnerPosition();
             CheckRaceCompletion();
         }
@@ -96,7 +95,6 @@ namespace _OlympicPulse.Scripts
                 Pose hitPose = hits[0].pose;
                 SpawnSprinter(hitPose.position);
                 initialPosition = spawnedSprinter.transform.position;
-                Debug.Log($"Sprinter Position after being placed: {initialPosition}");
             }
         }
 
@@ -106,10 +104,8 @@ namespace _OlympicPulse.Scripts
             {
                 Destroy(spawnedSprinter);
             }
-            // Orient the sprinter to face the right side of the screen
             Vector3 cameraRight = Camera.main.transform.right;
             Quaternion sprinterOrientation = Quaternion.LookRotation(cameraRight);
-    
             spawnedSprinter = Instantiate(SprinterPrefab, position, sprinterOrientation);
         }
 
@@ -120,36 +116,33 @@ namespace _OlympicPulse.Scripts
                 Debug.LogError("Sprinter is null. Cannot start race.");
                 return;
             }
-            Debug.Log("Starting race countdown.");
             raceCountdown = RaceCountdownDuration;
             totalDistanceCovered = 0.0f;
             raceTimer = 0.0f;
             raceEnded = false;
-            Debug.Log($"Sprinter Position at OnStartRaceButtonPressed(): {initialPosition}");
         }
 
         private void UpdateRunnerPosition()
         {
-            float distanceToCover = 100.0f;
-            float distanceCovered = WorldRecordPace * raceTimer;
-            float fractionOfJourney = distanceCovered / distanceToCover;
+            // Calculate time and speed for 20 metres based on the world record time for 100 metres
+            float timeFor20m = (20.0f / 100.0f) * WorldRecordTime100m;
+            float speedFor20m = 20.0f / timeFor20m;
 
-            Vector3 endPosition = initialPosition + spawnedSprinter.transform.forward * 100.0f;
-            Vector3 newPosition = Vector3.Lerp(initialPosition, endPosition, fractionOfJourney);
+            // Calculate the distance to move in this frame
+            float distanceToMove = speedFor20m * Time.deltaTime;
 
+            // Update the sprinter's position
+            Vector3 newPosition = spawnedSprinter.transform.position + spawnedSprinter.transform.forward * distanceToMove;
             spawnedSprinter.transform.position = newPosition;
-            totalDistanceCovered = Vector3.Distance(initialPosition, newPosition);
-
-            Debug.Log("New Position: " + newPosition);
+            
+            // Update total distance covered
+            totalDistanceCovered += distanceToMove;
         }
 
         private void CheckRaceCompletion()
         {
-            Debug.Log("Total Distance Covered: " + totalDistanceCovered);
-
-            if (totalDistanceCovered >= 100.0f && !raceEnded)
+            if (totalDistanceCovered >= 20.0f && !raceEnded)  // Changed to 20 metres
             {
-                Debug.Log("Ending Race.");
                 EndRace();
             }
         }
@@ -161,10 +154,6 @@ namespace _OlympicPulse.Scripts
                 Debug.LogError("Sprinter is null. Cannot end race.");
                 return;
             }
-            Debug.Log("Race ended. Sprinter should stop.");
-            Debug.Log($"Is Sprinter Active: {spawnedSprinter.activeSelf}");
-            Debug.Log($"Sprinter Position at Race End: {spawnedSprinter.transform.position}");
-
             spawnedSprinter.GetComponent<Actions>().Stay();
             raceEnded = true;
             raceTimer = 0;
